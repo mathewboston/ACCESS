1f3e5f7fcacb2ec7bf29197de4d8d08262bb5fbc
Gerardo Vela <Gerardo Vela>
2016-12-08 22:37:24 -0700

    added orsp, banner, database connection

---
+++ Servlet/database/Banner_Info.java
@@ -0,0 +1,11 @@
+/* Author: Gerardo Vela */
+/* Retrieves data from Banner */
+public class Banner_Info extends Banner_Connection
+{
+
+  // retrieve student info, hash map?
+  private void reTrieveInfo()
+  {
+
+  }
+}
---
+++ Servlet/database/ORSP_Connection.java
@@ -0,0 +1,21 @@
+/* Author: Gerardo Vela */
+/*Responsible for connection to ORSP Database */
+public class ORSP_Connection
+{
+  private String database;
+  private String username;
+  private String password;
+
+  public ORSP_Connection()
+  {
+    this.database="";
+    this.username="";
+    this.password="";
+  }
+
+// creates a connection to ORSP
+ public void createConnection(String database, String usrename, String password)
+ {
+   
+ }
+}
