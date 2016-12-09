fce37dc19f0befc91fe56a6e04de9ca38541ab8f
Gerardo Vela <Gerardo Vela>
2016-12-08 21:30:28 -0700

    added resource folder

---
+++ Servlet/resource/InitiativeAdministrator.java
@@ -0,0 +1,16 @@
+
+public class InitiativeAdministrator extends Member {
+	
+	public InitiativeAdministrator(){
+		super();
+	}
+	
+	//Link Activity to Initiative 
+	public void linkToInitiative(int initiativeID){
+		
+		//TODO: use this method from initiative
+		//TODO: This method returns a list<Activity>
+		getAssociateActivity(); 
+	}
+
+}
