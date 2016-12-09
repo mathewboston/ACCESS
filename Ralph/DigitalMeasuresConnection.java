6f6ec5712cabada82e2a0616d594b1c53295e684
RAWoiwode <rawoiwode@gmail.com>
2016-12-08 21:21:16 -0700

    Added Skeleton classes
    Added basic classes just so the team could see it

---
+++ Ralph/DigitalMeasuresConnection.java
@@ -0,0 +1,21 @@
+
+
+/**
+ *
+ * @author Ralph Woiwode
+ */
+public class DigitalMeasuresConnection {
+    private String website;
+    private String userName;
+    private String password;
+    
+    public DigitalMeasuresConnection(){
+        String website = "www.DMACCESS.com";
+        String userName = "DMTest";
+        String password = "DM2016";
+    }
+    
+    public void connectToDigitalMeasures(){
+        
+    }
+}
